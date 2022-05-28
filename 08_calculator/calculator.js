8dc5a07f48fcd7beec9987f6792c1b0245fe9337
Treyce Wheeler <t.zeryon@gmail.com>
2022-05-27 21:34:29 -0400

    Answer problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(...args) {
+  let answer = 0;
+  args.forEach(arg => {
+    answer += arg;
+  });
+  return answer;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	let answer = 0;
+  args.forEach(arg => {
+    
+    if (typeof(arg) === 'object'){
+      arg.forEach(num => {
+        answer += num;
+      });
+    }
+    
+    else {
+      answer += arg;
+    }
+  });
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = array.reduce(function(total, currentNumber){
+    //Reduce must return a number, the returned number becomes the new total
+    return total * currentNumber;
+  });
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(num1 , num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1
+  for (let i = 1; num > i; num--) {
+    answer *= num;
+  }
+  return answer;
 };
 
 // Do not edit below this line
