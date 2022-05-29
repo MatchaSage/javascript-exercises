0fa386e735a24dba4f9e5d118ebd36cf6876a518
Treyce Wheeler <t.zeryon@gmail.com>
2022-05-28 21:32:59 -0400

    Solve problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (sent) {
+    let regString = revString = sent.toLowerCase();
+    
+    fixedReg = fixedRev = regString.replace(/[^a-z]/g, '');
+    
+    fixedRev = fixedRev.split('').reverse();
+    fixedRev = fixedRev.join('');
+    
+    if (fixedRev == fixedReg) {
+        return true;
+    }   
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
