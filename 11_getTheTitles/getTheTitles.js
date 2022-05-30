2999ca40f66bddfe4a8356ecd574af44d1901a05
Treyce Wheeler <t.zeryon@gmail.com>
2022-05-29 20:35:57 -0400

    Answer problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(books) {
+    let emptArr = [];
+    for (let i = 0; i < books.length; i++) {
+        emptArr.push(books[i].title);
+    }
+    return emptArr;
+}
 
-};
-
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+      
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+const findTheOldest = function(people) {
+    let ageArray = [];
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            people[i].yearOfDeath = new Date().getFullYear();
+        }
+        
+        ageArray.push(people[i].yearOfDeath - people[i].yearOfBirth);
+    }
+
+    let oldest = Math.max(...ageArray);
+    let oldestIndex = ageArray.indexOf(oldest);
+    let thePerson = people[oldestIndex];
+    return thePerson;
+    
 };
 
+findTheOldest(people).name;
 // Do not edit below this line
 module.exports = findTheOldest;
